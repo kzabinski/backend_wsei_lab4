0ebe3d2298774a1165679b1836cecacec75c8fb1
Krzysztof Żabiński <krzysztofzabinski@Krzysztofs-MacBook-Air.local>
2025-03-26 22:57:49 +0100

    Adding simple GraphQL application

---
+++ src/main/java/com/kzabinski/backend_wsei_lab4/BackendWseiLab4Application.java
@@ -0,0 +1,13 @@
+package com.kzabinski.backend_wsei_lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWseiLab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWseiLab4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kzabinski/backend_wsei_lab4/BackendWseiLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kzabinski.backend_wsei_lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWseiLab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
